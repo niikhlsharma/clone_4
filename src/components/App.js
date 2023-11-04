45a91458efd2c84ec74742fa7f4cb27077d38776
niikhlsharma <65024567+niikhlsharma@users.noreply.github.com>
2023-11-05 01:36:11 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,15 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [buttonValue,setbuttonValue] = useState(false);
+  function toggle(){
+    setbuttonValue(!buttonValue)
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      
+    <button onClick={toggle}>click</button>
+  {buttonValue && <p>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
     </div>
   );
 }
