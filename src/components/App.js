0095b22737387987f0c371072c6b991cacceeb73
niikhlsharma <65024567+niikhlsharma@users.noreply.github.com>
2024-02-22 16:07:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,26 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [buttonValue,setbuttonValue] = useState(false);
-  function toggle(){
-    setbuttonValue(!buttonValue)
-  }
+ 
   return (
     <div id="main">
-      
-    <button id='click' onClick={toggle}>click</button>
-  {buttonValue && <p id='para'>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+
+    <button id='click' onClick={fun}>Render</button>
+
+    <p id='para'>
+
+
+    </p>
+   
     </div>
   );
 }
 
+const para = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
+  function fun()
+{
+   const abc = document.getElementById('para');
+   abc.innerHTML = para;
+}
 
 export default App;
